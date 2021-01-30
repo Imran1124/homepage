b8a23baf4b08bb5c220321a748a05792d0053574
Imran1124 <78031419+Imran1124@users.noreply.github.com>
2021-01-31 02:30:38 +0530

    Update Api.jsx

--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -1,11 +1,11 @@
-export const ApiUrl = "http://imran1124.pythonanywhere.com/movie/";
+export const ApiUrl = "https://imran1124.pythonanywhere.com/movie/";
 //const ApiUrl = "http://localhost:8000/movie/";
-export const countryApi="http://imran1124.pythonanywhere.com/country/";
+export const countryApi="https://imran1124.pythonanywhere.com/country/";
 
-export const stateApi="http://imran1124.pythonanywhere.com/state/?country_id=";
+export const stateApi="https://imran1124.pythonanywhere.com/state/?country_id=";
 
-export const cityApi="http://imran1124.pythonanywhere.com/city/?state_id=";
+export const cityApi="https://imran1124.pythonanywhere.com/city/?state_id=";
 
-export const singupApi='http://imran1124.pythonanywhere.com/singup/';
+export const singupApi='https://imran1124.pythonanywhere.com/singup/';
 
-export const edituser="http://imran1124.pythonanywhere.com/singup/";
+export const edituser="https://imran1124.pythonanywhere.com/singup/";
